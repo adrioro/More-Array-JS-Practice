feeabf4c3c2e27dc81953e92090ba92d310b31ab
adrioro <artistsvillage@me.com>
2019-01-14 14:43:40 -0500

    first commit push, shift, pot, indexing

---
+++ index.js
@@ -0,0 +1,6 @@
+var mynums = ['zero', 'one', 'two', 'three'];
+mynums[0] = "nine"; //Adds this items at index zero.
+mynums.push("twenty"); //Adds this item.
+mynums.push("zero", "thirteen"); //Adds these items.
+mynums.shift(); // Removes the first item and returns it.
+mynumbs.pop(); //Removes the last item and returns it.
