08f5ccf5b6ea3c9fc8b233c4784bbf4557eb42d8
adrioro <artistsvillage@me.com>
2019-01-14 14:57:59 -0500

    scond commit sort and splice

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ mynums[0] = "nine"; //Adds this items at index zero.
 mynums.push("twenty"); //Adds this item.
 mynums.push("zero", "thirteen"); //Adds these items.
 mynums.shift(); // Removes the first item and returns it.
-mynumbs.pop(); //Removes the last item and returns it.
+mynums.pop(); //Removes the last item and returns it.
+
+mynums.sort(); //if not specified it sorts it alphabetically.
+
+mynums.splice(0, 3); //it removes the items from the first index(0), to the second inde(3);
